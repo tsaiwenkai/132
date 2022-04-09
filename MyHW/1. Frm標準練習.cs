edbbec2d88ffdaf2b6e56f0aad4dd1086f3464a8
Gaming <Gaming@DESKTOP-I8R5ITP>
2022-04-09 14:07:54 +0800

    加入專案檔案。

---
+++ MyHW/1. Frm標準練習.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmHomeWork : Form
+    {
+        public FrmHomeWork()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] array = { 100, 66, 77 };
+            lblResult.Text = "最大數為:" + array.Max();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int odd = 0;
+            int even = 0;
+            int[] nums = { 33, 4, 5, 11, 222, 34 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+
+            }
+            lblResult.Text = "int[] nums = { 33, 4, 5, 11, 222, 34 };" + "偶數為:" + even + "  奇數為:" + odd;
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+            string[] names = { "aaa", "ksdkfjsdk", "asdasdasdasdas", "asda" };
+            int x = names[0].Length;
+            string max = "";
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (x <= names[i].Length)
+                {
+                    x = names[i].Length;
+                    max = names[i];
+                }
+            }
+            lblResult.Text = "名字最長的為:" + max;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(txtnum.Text);
+            if (x % 2 == 0)
+            {
+                lblResult.Text = "輸入的數  " + x + "為偶數";
+            }
+            else
+            {
+                lblResult.Text = "輸入的數  " + x + "為奇數";
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] scores = { 2, 3, 46, 33, 22, 100, 150, 33, 55 };
+
+            int max = scores.Max();
+
+            int min = scores.Min();
+            lblResult.Text = "最小值為:  " + min+ "最大值為:  " + max; ;
+            //MessageBox.Show("Max = " + max);
+
+            //Array.Sort(scores);
+            //MessageBox.Show("Max =" + scores[scores.Length - 1]);
+
+            //================================
+
+            //Point[] points = new Point[3];
+            //points[0].X = 3;
+            //points[0].Y = 4;
+            ////System.InvalidOperationException: '無法比較陣列中的兩個元素。'
+
+            //Array.Sort(points);
+
+            //=================================
+
+            //MessageBox.Show("Max = " + ClsUtility.MyMax(scores));
+
+        }
+
+        int MyMinScore(int[] nums)
+        {
+            return 10;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 2; j <= 9; j++)
+                {
+                    lblResult.Text += j + " x " + i + " = " + j * i + "  ";
+
+                }
+                lblResult.Text += "\n";
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string[] arr0771_Str = { "mother張", "emma", "迪克蕭", "J40", "Candy", "Cindy", "Coconut", "Motherfacker" };
+            int x = 0;
+            for (int i = 0; i < arr0771_Str.Length; i++)
+            {
+                foreach (char c in arr0771_Str[i])
+                {
+                    if (c == 'c' || c == 'C')
+                    {
+                        x++;
+                    }
+                }
+            }
+            lblResult.Text= "string[] arr0771_Str = { mother張, emma, 迪克蕭, J40, Candy, Cindd, Coconut, Motherfacker } 有C 及 c的名字共有 " + x + " 個";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            List<int> LS = new List<int>();
+            lblResult.Text = "";
+            for (int i = 1; i <= 6; i++)
+            {
+                int tmp = rd.Next(1, 49);
+                if (LS.Contains(tmp))  
+                {
+                    i--;
+                }
+                else
+                {
+                    LS.Add(tmp); 
+                    lblResult.Text += "  " + tmp.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ MyHW/1. Frm標準練習.designer.cs
@@ -0,0 +1,387 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class FrmHomeWork
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button19 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button16 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtnum = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(953, 17);
+            this.button19.Margin = new System.Windows.Forms.Padding(2);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(147, 30);
+            this.button19.TabIndex = 104;
+            this.button19.Text = "int[] 樂透 6個號碼不重複";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(123, 192);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 27);
+            this.label7.TabIndex = 101;
+            this.label7.Text = "Result:";
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(19, 192);
+            this.button16.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(79, 27);
+            this.button16.TabIndex = 100;
+            this.button16.Text = "清空結果欄";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("新細明體", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(123, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 27);
+            this.label5.TabIndex = 99;
+            // 
+            // txtnum
+            // 
+            this.txtnum.Location = new System.Drawing.Point(69, 26);
+            this.txtnum.Name = "txtnum";
+            this.txtnum.Size = new System.Drawing.Size(76, 22);
+            this.txtnum.TabIndex = 98;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 12);
+            this.label6.TabIndex = 97;
+            this.label6.Text = "Number:";
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(392, 61);
+            this.button15.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(117, 23);
+            this.button15.TabIndex = 96;
+            this.button15.Text = "100 的 二進位是多少 ?";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(546, 105);
+            this.button13.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(222, 39);
+            this.button13.TabIndex = 95;
+            this.button13.Text = "string[] 名字有 “C” or “c” 字樣有幾個 ";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(546, 64);
+            this.button14.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(209, 28);
+            this.button14.TabIndex = 94;
+            this.button14.Text = "string[] 最長名字 ?";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.Black;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Font = new System.Drawing.Font("Consolas", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.Lime;
+            this.lblResult.Location = new System.Drawing.Point(211, 189);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(79, 34);
+            this.lblResult.TabIndex = 93;
+            this.lblResult.Text = "結果";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(392, 24);
+            this.button12.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(117, 23);
+            this.button12.TabIndex = 92;
+            this.button12.Text = "九九乘法表";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button9.Location = new System.Drawing.Point(312, 101);
+            this.button9.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(56, 17);
+            this.button9.TabIndex = 91;
+            this.button9.Text = "do";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button10.Location = new System.Drawing.Point(312, 64);
+            this.button10.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(56, 17);
+            this.button10.TabIndex = 90;
+            this.button10.Text = "while";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button11.Location = new System.Drawing.Point(312, 25);
+            this.button11.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(56, 17);
+            this.button11.TabIndex = 89;
+            this.button11.Text = "for";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(167, 99);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 12);
+            this.label3.TabIndex = 88;
+            this.label3.Text = "Step:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(178, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 87;
+            this.label2.Text = "to:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(169, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 12);
+            this.label1.TabIndex = 86;
+            this.label1.Text = "from:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox3.Location = new System.Drawing.Point(211, 96);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 22);
+            this.textBox3.TabIndex = 85;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox2.Location = new System.Drawing.Point(211, 58);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 22);
+            this.textBox2.TabIndex = 84;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox1.Location = new System.Drawing.Point(211, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 22);
+            this.textBox1.TabIndex = 83;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(546, 21);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(209, 28);
+            this.button5.TabIndex = 78;
+            this.button5.Text = "int[] 統計 奇數 有幾個 , 偶數有幾個";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(785, 61);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 28);
+            this.button4.TabIndex = 77;
+            this.button4.Text = " MAX()   / Min ( int[] )";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(785, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 28);
+            this.button3.TabIndex = 76;
+            this.button3.Text = "Max (params int[] )";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 99);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 74;
+            this.button1.Text = "3 個數的最大值";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(29, 73);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 21);
+            this.button6.TabIndex = 73;
+            this.button6.Text = "Odd or Even";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // FrmHomeWork
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 439);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtnum);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button6);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FrmHomeWork";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtnum;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+    }
+}
